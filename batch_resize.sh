0f1af657f518c0e42fd6b98538b64ed0b9e05f7d
Andrew P Bergeron <andrewpbergeron@gmail.com>
2015-05-02 18:55:37 -0500

    nested loops could have been their own function. Oh well. its completed

--- batch_resize.sh
+++ batch_resize.sh
@@ -12,7 +12,10 @@ ratios="$3"
 
 echo $input_dir
 helpMe(){
+	echo "run '"chmod +x batch_resize.sh"' "
 	echo "Arg format: ./batch_resize.sh <input_dir> <output_dir> “[resize ratios]”"
+	echo "example: ./batch_resize.sh lfw file_out 50"
+	echo "THIS SCRIPT AUTOMATICALLY DELETES REDUNDANT DIRECTORIES"
 }
 
 #make the output directory. If the name exists, delete it and write over
@@ -25,30 +28,89 @@ fi
 	echo "Overwriting '"$output_dir"' is easier for testing rather than EXIT"
 }
 
-copy_input_to_outfile(){
-echo "inputfile:" "$input_dir"
-echo "outputfile:" "$output_dir"
+#mimic sub directory structure where the images are located
+make_output_dir(){
+#echo "Make output directory"
+#echo "inputfile:" "$input_dir"
+#echo "outputfile:" "$output_dir"
 
-#mv $input_dir/*/*jpg "$output_dir"
-#cp -r $input_dir "output_dir"
 for i in $input_dir/*/; do
-	echo $i
+	#echo "file name: "$i
 	mkdir $output_dir/$(basename $i)
-	for pic in $input_dir/$(basename $i)/*.jpg; do
-		echo $pic
-		#call resize function here and remove cp cmd
-		cp $pic $output_dir/$(basename $i)/
-
-	done
+	
 done
-
-
-}
-resize_image(){
-
-
 }
 
+#get jpegs and cut file names to insert the ratio identifier
+get_to_jpg_files(){
+
+for i in $input_dir/*/; do
+TotalFiles=$TotalFiles
+oSizeT=$oSizeT
+        for img in $input_dir/$(basename $i)/*.jpg; do
+	
+        echo "File name: "$img
+        filename=`echo $img | cut -d '.' -f 1`
+        fileext=`echo $img | cut -d '.' -f 2`
+        #echo "name:" "${filename}" 
+        #echo "ext:" "${fileext}"
+	oSize=`wc -c $img | awk '{print $1;}'`
+	echo "Size of File: "$oSize
+	oSizeT=$((oSizeT+$oSize))
+
+	getFileName=$(basename "$filename")
+	noPathFileName="${filename##*/}"
+#	echo "no path" "${noPathFileName}"
+        output_D=$(getOutputDirOfImg $img)/$noPathFileName
+        #echo "output_dir to put image: "$output_D
+	
+	
+
+                for ratio in $ratios; do
+                        #echo "orignal file=" $img
+			out_img="$output_D-r$ratio.$fileext"
+                        #echo "Resize: "$ratio
+			echo "new File:  " $out_img
+                        ReSize
+                         TotalFiles=$((TotalFiles + 1))
+			#convert -resize "$ratio%" $img $out_img
+                      # echo "out_img inside ratio loop: "$out_img
+                done
+        done
+done
+}
+
+ReSize(){
+	newSizeT=$newSizeT
+	for f in $output_D/*/; do
+	#	echo "image in Resize function=" $img
+	#	echo "output_D in Resize function=" $output_D
+	#	echo "out_img in Resize function=" $out_img
+		convert -resize "$ratio%" $img $out_img
+		 newSize=`wc -c $out_img | awk '{print $1;}'`
+		echo "Size of New File: " $newSize
+		newSizeT=$((newSizeT+$newSize))
+	done
+
+}
+
+
+getOutputDirOfImg(){
+	echo $output_dir/$(basename $(basename $(dirname $1)))
+}
+
+
+print_results(){
+	#wc -c <file_name> | awk '{print $1}' ---- wc -c gets file in bytes,
+	#pipes the pattern on first arg (file_name) and prints bytes size
+	#oSizeT=$(du -hs $filename)
+	#newSizeT=$(du -h $out_img)
+	echo "---------------------------------------------------"
+	echo "Total Files: "$TotalFiles
+	echo "Total Size of input Files: " $oSizeT
+	echo "Total Size of output Files: " $newSizeT
+	#printf "%-20s | %-20s | %-5s | %-5s | %-5s | %-5s" $filename $out_img $oSize $newSize $oSizeT $newSizeT
+}
 
 
 if [ "$1" == "help" ]; then
@@ -59,7 +121,6 @@ fi
 
 
 mkdir_output
-copy_input_to_outfile
-resize_image
-
-
+make_output_dir
+get_to_jpg_files
+print_results
